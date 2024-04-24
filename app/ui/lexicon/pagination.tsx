ebeb2938d1fd054c2327b853ce551d6294fd1b97
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-04-24 19:49:43 +0200

    enable pagination

--- app/ui/lexicon/pagination.tsx
+++ app/ui/lexicon/pagination.tsx
@@ -11,13 +11,14 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
-  const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   }
+  
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
@@ -66,8 +67,8 @@ function PaginationNumber({
 }: {
   page: number | string;
   href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
   isActive: boolean;
+  position?: 'first' | 'last' | 'middle' | 'single';
 }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center text-sm border',
