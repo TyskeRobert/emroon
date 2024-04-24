5eef1dde8e5f10ec9bde56431cc765714c4f5247
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-04-24 18:29:20 +0200

    update skeletons; install use-debounce

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -105,36 +105,38 @@ export default function DashboardSkeleton() {
 export function TableRowSkeleton() {
   return (
     <tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
-      {/* Customer Name and Image */}
+      {/* Customer Name and Image
       <td className="relative overflow-hidden whitespace-nowrap py-3 pl-6 pr-3">
         <div className="flex items-center gap-3">
           <div className="h-8 w-8 rounded-full bg-gray-100"></div>
           <div className="h-6 w-24 rounded bg-gray-100"></div>
         </div>
       </td>
-      {/* Email */}
-      <td className="whitespace-nowrap px-3 py-3">
+      */}
+      {/* ID */}
+      <td className="whitespace-nowrap px-3 py-3 w-1 sm:pl-6">
+        <div className="h-6 w-16 rounded bg-gray-100"></div>
+      </td>
+      {/* Entry */}
+      <td className="whitespace-nowrap px-3 py-3 w-1">
         <div className="h-6 w-32 rounded bg-gray-100"></div>
       </td>
-      {/* Amount */}
+      {/* Part of speech */}
       <td className="whitespace-nowrap px-3 py-3">
         <div className="h-6 w-16 rounded bg-gray-100"></div>
       </td>
-      {/* Date */}
+      {/* ONP */}
       <td className="whitespace-nowrap px-3 py-3">
         <div className="h-6 w-16 rounded bg-gray-100"></div>
       </td>
-      {/* Status */}
-      <td className="whitespace-nowrap px-3 py-3">
-        <div className="h-6 w-16 rounded bg-gray-100"></div>
-      </td>
-      {/* Actions */}
+      {/* Actions
       <td className="whitespace-nowrap py-3 pl-6 pr-3">
         <div className="flex justify-end gap-3">
           <div className="h-[38px] w-[38px] rounded bg-gray-100"></div>
           <div className="h-[38px] w-[38px] rounded bg-gray-100"></div>
         </div>
       </td>
+      */}
     </tr>
   );
 }
@@ -171,46 +173,47 @@ export function LexiconTableSkeleton() {
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            <LexiconTableMobileSkeleton />
-            <LexiconTableMobileSkeleton />
-            <LexiconTableMobileSkeleton />
-            <LexiconTableMobileSkeleton />
-            <LexiconTableMobileSkeleton />
-            <LexiconTableMobileSkeleton />
+              {(() => {
+                let r = [];
+                for (let i = 0; i < 10; i++) {
+                  r.push(<LexiconTableMobileSkeleton key={i}/>);
+                }
+                return r;
+              })()}
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                  ID
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Email
+                  Entry
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Amount
+                  Part of speech
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Date
+                  ONP
                 </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Status
-                </th>
+                {/*
                 <th
                   scope="col"
                   className="relative pb-4 pl-3 pr-6 pt-2 sm:pr-6"
                 >
                   <span className="sr-only">Edit</span>
                 </th>
+                */}
               </tr>
             </thead>
             <tbody className="bg-white">
-              <TableRowSkeleton />
-              <TableRowSkeleton />
-              <TableRowSkeleton />
-              <TableRowSkeleton />
-              <TableRowSkeleton />
-              <TableRowSkeleton />
+              {(() => {
+                let r = [];
+                for (let i = 0; i < 10; i++) {
+                  r.push(<TableRowSkeleton key={i}/>);
+                }
+                return r;
+              })()}
             </tbody>
           </table>
         </div>
