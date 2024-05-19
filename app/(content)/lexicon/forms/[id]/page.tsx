db12993689bd6c33ed73df83e341912322f25710
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-05-19 11:08:57 +0200

    rudimentary data for morphological analysis

--- app/(content)/lexicon/forms/[id]/page.tsx
+++ app/(content)/lexicon/forms/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { fetchForm } from "@/app/lib/data";
+import FormalAnalysis from "@/app/ui/lexicon/analysis";
 
 export default async function Page({ params }: { params: { id: string }}) {
     const id = params.id;
@@ -7,7 +8,7 @@ export default async function Page({ params }: { params: { id: string }}) {
         <div className="mt-5">
             <div className="text-gray-500">{form.id}</div>
             <h2 className="text-2xl font-bold italic">{form.norm}</h2>
-            <p className="text-xl">Form: {form.morph}</p>
+            <FormalAnalysis morph={form.morph}/>
         </div>
     );
 }
---
+++ app/ui/lexicon/analysis.tsx
@@ -0,0 +1,151 @@
+export default function FormalAnalysis({ morph } : { morph: string }) {
+    const state = (s => {
+        switch (s) {
+            case "f":
+                return "finite";
+            case "i":
+                return "infinitive";
+            case "r":
+                return "resultative";
+            case "s":
+                return "supinum";
+            case "p":
+                return "participle";
+            case "g":
+                return "gerundive";
+            default:
+                return "";
+        }
+    })(morph[0]);
+
+    const tense = (s => {
+        switch (s) {
+            case "s":
+                return "present";
+            case "t":
+                return "preterite";
+            default:
+                return "";
+        }
+    })(morph[1]);
+
+    const mood = (s => {
+        switch (s) {
+            case "i":
+                return "indicative";
+            case "o":
+                return "optative";
+            case "p":
+                return "imperative";
+            default:
+                return "";
+        }
+    })(morph[2]);
+
+    const person = (s => {
+        switch (s) {
+            case "1":
+                return "1st";
+            case "2":
+                return "2nd";
+            case "3":
+                return "3rd";
+            default:
+                return "";
+        }
+    })(morph[3]);
+
+    const grade = (s => {
+        switch (s) {
+            case "p":
+                return "positive";
+            case "c":
+                return "comparative";
+            case "s":
+                return "superlative";
+            case "e":
+                return "equative";
+            default:
+                return "";
+        }
+    })(morph[4]);
+
+    const gender = (s => {
+        switch (s) {
+            case "m":
+                return "masculine";
+            case "f":
+                return "feminine";
+            case "n":
+                return "neuter";
+            case "x":
+                return "unspecified";
+            default:
+                return "";
+        }
+    })(morph[5]);
+
+    const number = (s => {
+        switch (s) {
+            case "s":
+                return "singular";
+            case "p":
+                return "plural";
+            default:
+                return "";
+        }
+    })(morph[6]);
+
+    const casus = (s => {
+        switch (s) {
+            case "n":
+                return "nominative";
+            case "g":
+                return "genitive";
+            case "d":
+                return "dative";
+            case "a":
+                return "accusative";
+            case "x":
+                return "unspecified";
+            default:
+                return "";
+        }
+    })(morph[7]);
+
+    const specificity = (s => {
+        switch (s) {
+            case "i":
+                return "indefinite";
+            case "d":
+                return "definite";
+            default:
+                return "";
+        }
+    })(morph[8]);
+
+    return (
+        <table>
+            <tbody>
+                <CategoryRow label="State" value={state}/>
+                <CategoryRow label="Tense" value={tense}/>
+                <CategoryRow label="Mood" value={mood}/>
+                <CategoryRow label="Person" value={person}/>
+                <CategoryRow label="Grade" value={grade}/>
+                <CategoryRow label="Gender" value={gender}/>
+                <CategoryRow label="Number" value={number}/>
+                <CategoryRow label="Case" value={casus}/>
+                <CategoryRow label="Specificity" value={specificity}/>
+            </tbody>
+        </table>
+    )
+}
+
+function CategoryRow({ label, value }: { label: string, value: string }) {
+    return value ? (
+        <tr key={label}>
+            <th>{label}:</th>
+            <td>{value}</td>
+        </tr>
+    ) : null;
+}
