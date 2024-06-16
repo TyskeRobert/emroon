6a35c940db8e961b05c60654cd59cd85c4dbf76b
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-06-16 11:25:55 +0200

    fix bug retrieving vocab items

--- app/(content)/lexicon/lemmata/[id]/page.tsx
+++ app/(content)/lexicon/lemmata/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({ params }: { params: { id: string }}) {
         <div className="mt-5">
             <div className="text-gray-500">{lemma.id}</div>
             <h2 className="text-2xl font-bold italic">{lemma.entry}</h2>
-            <p className="text-xl">{partsOfSpeech.get(lemma.pos) || `Undefined part of speech: ${lemma.pos}`}</p>
+            <p className="text-xl">{partsOfSpeech.get(lemma.pos)?.full || `Undefined part of speech: ${lemma.pos}`}</p>
             <Paradigm lemma={lemma}/>
             <GlossONP id={lemma.linkONP}/>
         </div>
